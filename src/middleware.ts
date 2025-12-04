00ad24b0b2c2ace6df8f8543079c9977be497269
Oscar-Galindo <onmgalindo@gmail.com>
2025-12-04 00:50:06 -0500

    Remove middleware file completely - no license validation needed

--- src/middleware.ts
+++
@@ -1,7 +0,0 @@
-import { defineMiddleware } from 'astro:middleware';
-
-// Simple pass-through middleware - no license validation for fundraiser
-export const onRequest = defineMiddleware(async (context, next) => {
-  return next();
-});
-
